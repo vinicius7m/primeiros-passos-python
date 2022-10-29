8fe3c78e2a53b21aeaeb7176a4fc71ec6192d444
Vinicius Moreira <vinicius.vsm777@gmail.com>
2022-10-29 11:45:07 -0300

    Adding number random

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,27 +1,37 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************")
 
-numero_secreto = 43
+numero_secreto = round(random.random() * 100)
+print(numero_secreto)
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite seu número:  ")
-chute = int(chute_str)
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite seu número:  ")
+    chute = int(chute_str)
 
-print("Voce digitou ", chute)
-print(type(chute))
+    print("Voce digitou ", chute)
+    print(type(chute))
 
-acertou = chute == numero_secreto
-maior   = chute > numero_secreto
-menor   = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior   = chute > numero_secreto
+    menor   = chute < numero_secreto
 
-if (acertou):
-    print("*************")
-    print("Você acertou!")
-    print("*************")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if (acertou):
+        print("*************")
+        print("Você acertou!")
+        print("*************")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do jogo!")
